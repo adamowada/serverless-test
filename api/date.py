83806280976a8eb930359a0b6b61970b689884a5
Adam Owada <adamowada@gmail.com>
2023-01-09 08:51:34 -0800

    lowercase h in classes

--- api/date.py
+++ api/date.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 
 
-class Handler(BaseHTTPRequestHandler):
+class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
